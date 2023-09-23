81b71a5062ddbc2e65ad39048c734c5ca86b5fa2
Timur <timazizov@gmail.com>
2023-09-23 11:48:40 +0600

    Initial commit

---
+++ src/main/java/com/halykhackathon/halykanalitycs/HalykAnalitycsApplication.java
@@ -0,0 +1,13 @@
+package com.halykhackathon.halykanalitycs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalykAnalitycsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalykAnalitycsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/halykhackathon/halykanalitycs/HalykAnalitycsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halykhackathon.halykanalitycs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalykAnalitycsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
